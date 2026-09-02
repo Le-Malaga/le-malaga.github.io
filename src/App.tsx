99c1bd5c9afaa25dae67d314b907f5395b7592c7
Elijah Mondero <elijahmondero@gmail.com>
2026-09-02 22:47:00 +1200

    Switch to BrowserRouter and add 404.html hack for SPA routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import Placeholder from './pages/Placeholder'
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="*" element={<Placeholder />} />
         </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
 
